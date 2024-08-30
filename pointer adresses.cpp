e13159a0529e9d55f86f07d6e3936cb1d5ace0b9
aryahule <aryahule27@gmail.com>
2024-08-30 12:21:09 +0530

    Add files via upload

---
+++ pointer adresses.cpp
@@ -0,0 +1,21 @@
+//Name:AryaHule
+//PRN:23070123029
+//Aim: To print the address of the variable usng pointers.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *aptr = &a;
+    cout << *aptr << endl;
+    cout << aptr << endl;
+    cout << &a << endl;
+
+    return 0;
+}
+/*
+Output
+10
+0x7ffccf44bba4
+0x7ffccf44bba4
+*/
